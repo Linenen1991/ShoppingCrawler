6225d5eae92b04ef04900e4d61f4d4a6b61d2686
Your Name <you@example.com>
2021-05-08 02:30:49 +0800

    debug mode

--- GetrutenHTML.py
+++ GetrutenHTML.py
@@ -1,14 +1,19 @@
+#!/usr/bin/python3
 from selenium import webdriver
-import time
 
 
 chrome_options = webdriver.ChromeOptions()
 prefs = {"profile.managed_default_content_settings.images": 2}
 chrome_options.add_experimental_option("prefs", prefs)
-driver = webdriver.Chrome(chrome_options=chrome_options)
+chrome_options.add_argument('--headless')
+driver = webdriver.Chrome(options=chrome_options)
 # driver.get('https://www.ruten.com.tw/find/?q=%e6%9e%97%e7%be%a9%e9%9b%84&sort=new%2Fdc');
 driver.get('https://www.google.com');
 
-
-print(driver.page_source)
-driver.close()
+try:
+    cmd=input('Enter your command\n')
+    while cmd != 'Q':
+        eval(cmd)
+        cmd = input('Enter your command\n')
+finally:
+    driver.close()
